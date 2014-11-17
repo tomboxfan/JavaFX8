0c95a43046e03a971c0531d179b99922c9aa199a
Tom Fan <tom.fan@mnjcap.com>
2014-11-17 11:35:57 +0800

    updating source

--- src/main/java/chap04/HBoxExample.java
+++ src/main/java/chap04/HBoxExample.java
@@ -15,10 +15,14 @@ public class HBoxExample extends Application {
         launch(args);
     }    
     @Override public void start(Stage primaryStage) {
-        //1) primaryStage之上是scene
-        //2) 创建scene需要一个group对象(root)
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 250);
+        
+        //1) 第1层 - 最上面是group - 你所有的东西都添加到group里面
+        Group group = new Group();
+        
+        //2) 第2层 - group下面是scene
+        Scene scene = new Scene(group, 300, 250);
+        
+        //3) 第3层 - scene下面才是传入的primaryStage
         primaryStage.setScene(scene);
         primaryStage.setTitle("HBox Example");        
 //从这往上，全都忽略----------------------------------------------------------------------------------------                
@@ -41,8 +45,10 @@ public class HBoxExample extends Application {
         //step 3) ----将4个rectangle添加到hbox中---------------------------------------
         hbox.getChildren().addAll(r1, r2, r3, r4);
         //-------------------------------------------------------------------------
-        
-        root.getChildren().add(hbox);
+
+        //step 3) ----将hbox添加到group中---------------------------------------------
+        group.getChildren().add(hbox);
+      //---------------------------------------------------------------------------
 
         primaryStage.setOnShown((WindowEvent we) -> {
             System.out.println("hbox width  " + hbox.getBoundsInParent().getWidth());  //宽度=76. 4个rectangle - (2 + 10 + 2) + 20 + 5 + 20 = 59; 3个spacing - 5*3 = 15; padding左右各1. 一共76
