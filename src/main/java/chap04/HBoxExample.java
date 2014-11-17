d37d2e114454266ac71e7d96c4135feca5b9af33
Tom Fan <tom.fan@mnjcap.com>
2014-11-17 11:27:01 +0800

    updating code

--- src/main/java/chap04/HBoxExample.java
+++ src/main/java/chap04/HBoxExample.java
@@ -15,33 +15,42 @@ public class HBoxExample extends Application {
         launch(args);
     }    
     @Override public void start(Stage primaryStage) {
-        primaryStage.setTitle("HBox Example");        
+        //1) primaryStage之上是scene
+        //2) 创建scene需要一个group对象(root)
         Group root = new Group();
         Scene scene = new Scene(root, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("HBox Example");        
 //从这往上，全都忽略----------------------------------------------------------------------------------------                
         
+        //1) 注意Padding和Margin之间的区别。前者是整体，后者是个体
 
-        HBox hbox = new HBox(5); // pixels space between child nodes
-        hbox.setPadding(new Insets(1)); // padding between child nodes only
+        //step 1) ----定义hbox----------------------------------------------
+        HBox hbox = new HBox(5); //Spacing = 5. 表示nodes之间的距离
+        hbox.setPadding(new Insets(1)); // padding是整个HBox周围的一圈
+        //--------------------------------------------------------
+        
+        //step 2) ----定义添加到hbox的4个rectangle-------------------------------------
         Rectangle r1 = new Rectangle(10, 10);
+        HBox.setMargin(r1, new Insets(2, 2, 2, 2)); //Margin是特指某一个node周围的一圈
         Rectangle r2 = new Rectangle(20, 20);
         Rectangle r3 = new Rectangle(5, 20);
         Rectangle r4 = new Rectangle(20, 5);
-
-        HBox.setMargin(r1, new Insets(2, 2, 2, 2));
-
+        //-------------------------------------------------------------------------
+        
+        //step 3) ----将4个rectangle添加到hbox中---------------------------------------
         hbox.getChildren().addAll(r1, r2, r3, r4);
-
+        //-------------------------------------------------------------------------
+        
         root.getChildren().add(hbox);
 
         primaryStage.setOnShown((WindowEvent we) -> {
-            System.out.println("hbox width  " + hbox.getBoundsInParent().getWidth());
-            System.out.println("hbox height " + hbox.getBoundsInParent().getHeight());
+            System.out.println("hbox width  " + hbox.getBoundsInParent().getWidth());  //宽度=76. 4个rectangle - (2 + 10 + 2) + 20 + 5 + 20 = 59; 3个spacing - 5*3 = 15; padding左右各1. 一共76
+            System.out.println("hbox height " + hbox.getBoundsInParent().getHeight()); //高度=22. 最高的rectangle = 20; padding上下各1，一共22
         });
 
         
 //从这往下，全都忽略----------------------------------------------------------------------------------------              
-        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
